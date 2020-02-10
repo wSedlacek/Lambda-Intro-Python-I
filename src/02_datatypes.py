db26e57aa63d45b228e9d670b666d3dcacae0a0f
William Sedlacek <wsedlacekc@gmail.com>
2020-02-10 09:08:39 -0600

    Data Types
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -13,9 +13,12 @@ y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 
-# YOUR CODE HERE
+total = int(x) + int(y)
+print(total)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+string = str(x) + str(y)
+print(string)
